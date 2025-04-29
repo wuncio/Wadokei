6429acf75b7c60e6479e6049a93988a2dd84e436
Przemek-lab <s210999@sggw.edu.pl>
2025-04-29 17:29:45 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from sys import exit
+from sun_rise_set import sun_set_raise
+from location_calc import lat_long
+import ipapi
+
+print("Wprowadź nazwę miejsca w którym się znajdujesz (Jeżeli chcesz wyszukać miejsce automatycznie wpisz 1)")
+name = input("")
+if name == "1":
+    try:
+        ip = ipapi.location()
+    except:
+        print("Brak internetu")
+        exit(0)
+    city = ip["city"]
+    region = ip["region"]
+    country = ip["country_name"]
+    place = f"{city} {region} {country}"
+else:
+    place = name
+
+coords = lat_long(place)
+if coords == 0:
+    print("Podana lokalizacja nie może zostać wyszukana")
+elif coords == 1:
+    print("Brak internetu")
+else:
+    print(coords[0])
+    latitude = float(coords[1])
+    longitude = float(coords[2])
+    sun = sun_set_raise(name, latitude, longitude)
+    if not sun:
+        print("W tej lokacji słońce nie wschodzi lub nie zachodzi") #Arrival Heights Laboratory, Zackenberg grenlandia
+    else:
+        print("Wschód słońca:", format(sun[0], '%H:%M:%S'))
+        print("Zachód słońca:", format(sun[1], '%H:%M:%S'))
+        print("Długość dnia (w godz):", sun[2])
+        print("Długość nocy (w godz):", sun[3])
+        print("Długość nocy (w godz):", sun[3]/2)
+        print("Długość nocy (w godz):", 24 - sun[3] / 2)
+        print("Długość godziny dziennej (wadoke):", sun[4])
+        print("Długość godziny nocnej (wadoke):", sun[5])
